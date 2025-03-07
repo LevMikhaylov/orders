7026b315bfbdaf4f0ecee328bec3def72f91aea2
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2025-03-07 15:54:45 +0300

    Delete orders directory

--- orders/src/main/java/com/example/orders/Repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.orders.Repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.orders.Entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- orders/src/main/java/com/example/orders/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.orders.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.orders.Entities.Validation.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
