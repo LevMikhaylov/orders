06517edb501abf70bbea05efcfbf7d0ba7deb526
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2025-03-13 16:25:49 +0300

    Delete orders/orders directory

--- orders/orders/src/main/java/com/example/orders/Front/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App2 from './App2.jsx';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App2/>
-  </React.StrictMode>
-);
